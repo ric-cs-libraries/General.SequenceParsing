26510d876056f56a720fd8db3fb0ed465a7c06cd
Richard PAYET <rick.info.rsoc@gmail.com>
2024-02-14 20:08:18 +0100

    TESTS - First commit

---
+++ TESTS/Transverse._Common.SequenceParsing.UnitTests/Char/Sequence/Parser/CharSequenceParserTests.cs
@@ -0,0 +1,284 @@
+using System.Text;
+using Xunit;
+
+using Transverse._Common.SequenceParsing.Generic;
+using Transverse._Common.SequenceParsing.Char;
+using System.Text.RegularExpressions;
+using System.Runtime.Serialization;
+
+namespace Transverse._Common.SequenceParsing.UnitTests.Char;
+
+public class CharSequenceParserTests
+{
+    [Fact]
+    public void Create_WhenCalled_ShouldReturnAnInstanceOfCharSequenceParser()
+    {
+        var charSequence = CharSequence.Create("xxx");
+        var sut = CharSequenceParser.Create(charSequence);
+        Assert.IsType<CharSequenceParser>(sut);
+    }
+
+    [Fact]
+    public void Parse_WhenCalled_ShouldReturnTheCorrecteParseResult_BasicString1()
+    {
+        //--- Arrange ---
+        string stringToParse = "xyzAAwktAAFbzBBejAA123AAF567BBFrien";
+        CharSequenceParser parser = Fixtures.GetParser(stringToParse);
+
+        StringMatchingEvaluator stringMatchingEvaluator = new StringMatchingEvaluatorByEquality();
+
+        CharBlockDelimiter blockAStartDelimiter = CharBlockDelimiter.Create("AA", stringMatchingEvaluator);
+        CharBlockDelimiter blockAEndDelimiter = CharBlockDelimiter.Create("AAF", stringMatchingEvaluator);
+        CharBlock blockA = CharBlock.Create(blockAStartDelimiter, blockAEndDelimiter);
+
+        CharBlockDelimiter blockBStartDelimiter = CharBlockDelimiter.Create("BB", stringMatchingEvaluator);
+        CharBlockDelimiter blockBEndDelimiter = CharBlockDelimiter.Create("BBF", stringMatchingEvaluator);
+        CharBlock blockB = CharBlock.Create(blockBStartDelimiter, blockBEndDelimiter, expectedInnerBlocks: CharBlocks.Create(new List<CharBlock> { blockA }));
+
+        CharBlocks expectedInnerBlocks = CharBlocks.Create(new List<CharBlock> { blockA, blockB });
+
+
+
+        //--- Act ---
+        ParseResult<char> parseResult = parser.Parse(expectedInnerBlocks);
+        string resultAsString = parseResult.GetStateAsString();
+        //File.WriteAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_BasicString1.txt", resultAsString);
+
+        //--- Assert ---
+        string expectedResultAsString = File.ReadAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_BasicString1.txt");
+        Assert.Equal(expectedResultAsString, resultAsString);
+    }
+
+
+    [Fact]
+    public void Parse_WhenCalled_ShouldReturnTheCorrecteParseResult_From_BasicString2()
+    {
+        //--- Arrange ---
+        string stringToParse = "xyzAA012wktAaf7ZbzBBPejAA432123AAF8x567Bf .rien";
+        CharSequenceParser parser = Fixtures.GetParser(stringToParse);
+
+        StringMatchingEvaluator stringMatchingEvaluator = new StringMatchingEvaluatorByRegExp();
+
+        CharBlockDelimiter blockAStartDelimiter = CharBlockDelimiter.Create("AA[0-9]{3}", stringMatchingEvaluator);
+        CharBlockDelimiter blockAEndDelimiter = CharBlockDelimiter.Create("AAF[0-9]{1}(X|Z)", stringMatchingEvaluator, false);
+        CharBlock blockA = CharBlock.Create(blockAStartDelimiter, blockAEndDelimiter);
+
+        CharBlockDelimiter blockBStartDelimiter = CharBlockDelimiter.Create("BB(K|P)", stringMatchingEvaluator);
+        CharBlockDelimiter blockBEndDelimiter = CharBlockDelimiter.Create("BF [.]", stringMatchingEvaluator, false);
+        CharBlock blockB = CharBlock.Create(blockBStartDelimiter, blockBEndDelimiter, expectedInnerBlocks: CharBlocks.Create(new List<CharBlock> { blockA }));
+
+        CharBlocks expectedInnerBlocks = CharBlocks.Create(new List<CharBlock> { blockA, blockB });
+
+
+
+        //--- Act ---
+        ParseResult<char> parseResult = parser.Parse(expectedInnerBlocks);
+        string resultAsString = parseResult.GetStateAsString();
+        //File.WriteAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_BasicString2.txt", resultAsString);
+
+        //--- Assert ---
+        string expectedResultAsString = File.ReadAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_BasicString2.txt");
+        Assert.Equal(expectedResultAsString, resultAsString);
+    }
+
+
+    [Fact]
+    public void Parse_WhenStartDelimiterIdemAsEndDelimiterAndNotSelfNestable_ShouldReturnTheCorrecteParseResult()
+    {
+        //--- Arrange ---
+        string stringToParse = "xyAAzAA123AAAbzeAA456AAjAA123F567";
+        CharSequenceParser parser = Fixtures.GetParser(stringToParse);
+
+        StringMatchingEvaluator stringMatchingEvaluator = new StringMatchingEvaluatorByEquality();
+
+        CharBlockDelimiter blockAAStartDelimiter = CharBlockDelimiter.Create("AA", stringMatchingEvaluator);
+        CharBlockDelimiter blockAAEndDelimiter = blockAAStartDelimiter;
+        CharBlock blockAA = CharBlock.Create(blockAAStartDelimiter, blockAAEndDelimiter);
+
+        CharBlocks expectedInnerBlocks = CharBlocks.Create(new List<CharBlock> { blockAA });
+
+
+
+        //--- Act ---
+        ParseResult<char> parseResult = parser.Parse(expectedInnerBlocks);
+        string resultAsString = parseResult.GetStateAsString();
+        //File.WriteAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_StartDelimiterIdemAsEndDelimiterAndNOTSelfNestable.txt", resultAsString);
+
+        //--- Assert ---
+        string expectedResultAsString = File.ReadAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_StartDelimiterIdemAsEndDelimiterAndNOTSelfNestable.txt");
+        Assert.Equal(expectedResultAsString, resultAsString);
+    }
+
+    [Fact]
+    public void Parse_WhenCalled_ShouldReturnTheCorrecteParseResult_From_CSharpCodeAsString()
+    {
+        //--- Arrange ---
+        string stringToParse = """
+            // Ceci est un comm mono-ligne
+            /* Ceci est un commentaire 
+               multilignes
+               class pas { pris en compte car dans comm } .
+               */
+           
+               class MaClasse {
+              
+                  //Contenu de ma classe! class dans comm!
+
+                  if (toto == 9) {
+
+                    //Un comm mono
+                      if (bidule < 5) 
+                      {
+
+                        //C'est cool
+                      }
+
+                  }
+
+
+               }
+            """;
+        CharSequenceParser parser = Fixtures.GetParser(stringToParse);
+
+        CharBlocks expectedInnerBlocks = Fixtures.CSharpCode.expectedInnerBlocks;
+
+
+
+        //--- Act ---
+        ParseResult<char> parseResult = parser.Parse(expectedInnerBlocks);
+        string resultAsString = parseResult.GetStateAsString();
+        //File.WriteAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_FromStringAsCSharpCode.txt", resultAsString);
+
+        //--- Assert ---
+        string expectedResultAsString = File.ReadAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_FromStringAsCSharpCode.txt");
+        Assert.Equal(expectedResultAsString, resultAsString);
+    }
+
+    [Fact]
+    public void Parse_WhenCalled_ShouldReturnTheCorrecteParseResult_From_CSharpCodeFile()
+    {
+        //--- Arrange ---
+        string stringToParse = File.ReadAllText($"{Fixtures.Pathes.FilesToParse_SubPath}/CSharpCode.csharp");
+        CharSequenceParser parser = Fixtures.GetParser(stringToParse);
+
+        CharBlocks expectedInnerBlocks = Fixtures.CSharpCode.expectedInnerBlocks;
+
+
+
+        //--- Act ---
+        ParseResult<char> parseResult = parser.Parse(expectedInnerBlocks);
+        string resultAsString = parseResult.GetStateAsString();
+        //File.WriteAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_FromCSharpCodeFile.txt", resultAsString);
+
+        //--- Assert ---
+        string expectedResultAsString = File.ReadAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_FromCSharpCodeFile.txt");
+        Assert.Equal(expectedResultAsString, resultAsString);
+    }
+
+    [Fact]
+    public void Parse_WhenCalled_ShouldReturnTheCorrecteParseResult_From_XMLFile()
+    {
+        //--- Arrange ---
+        string stringToParse = File.ReadAllText($"{Fixtures.Pathes.FilesToParse_SubPath}/XML.xml");
+        CharSequenceParser parser = Fixtures.GetParser(stringToParse);
+
+        CharBlocks expectedInnerBlocks = Fixtures.XML.expectedInnerBlocks;
+
+
+
+        //--- Act ---
+        ParseResult<char> parseResult = parser.Parse(expectedInnerBlocks);
+        string resultAsString = parseResult.GetStateAsString();
+        //File.WriteAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_FromXMLFile.txt", resultAsString);
+
+        //--- Assert ---
+        string expectedResultAsString = File.ReadAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_FromXMLFile.txt");
+        Assert.Equal(expectedResultAsString, resultAsString);
+    }
+
+    [Fact]
+    public void Parse_WhenCalled_ShouldReturnTheCorrecteParseResult_From_XMLFileWithAttributes()
+    {
+        //--- Arrange ---
+        string stringToParse = File.ReadAllText($"{Fixtures.Pathes.FilesToParse_SubPath}/XML_WithAttributes.xml");
+        CharSequenceParser parser = Fixtures.GetParser(stringToParse);
+
+        CharBlocks expectedInnerBlocks = Fixtures.XML.expectedInnerBlocks;
+
+
+
+        //--- Act ---
+        ParseResult<char> parseResult = parser.Parse(expectedInnerBlocks);
+        string resultAsString = parseResult.GetStateAsString();
+        //File.WriteAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_FromXMLFile_WithAttributes.txt", resultAsString);
+
+        //--- Assert ---
+        string expectedResultAsString = File.ReadAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_FromXMLFile_WithAttributes.txt");
+        Assert.Equal(expectedResultAsString, resultAsString);
+    }
+
+    [Fact]
+    public void Parse_WhenCalled_ShouldReturnTheCorrecteParseResult_From_LPDP_MyPoemsList()
+    {
+        //--- Arrange ---
+        string stringToParse = File.ReadAllText($"{Fixtures.Pathes.FilesToParse_SubPath}/LPDP_MyPoemsList.html", Encoding.Latin1);
+        CharSequenceParser parser = Fixtures.LPDP_PoemsList.Parser(stringToParse);
+
+        CharBlocks expectedInnerBlocks = Fixtures.LPDP_PoemsList.expectedInnerBlocks;
+
+
+
+        //--- Act ---
+        ParseResult<char> parseResult = parser.Parse(expectedInnerBlocks);
+        string resultAsString = parseResult.GetStateAsString();
+        //File.WriteAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_From_LPDP_MyPoemsList.txt", resultAsString);
+
+        //--- Assert ---
+        string expectedResultAsString = File.ReadAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_From_LPDP_MyPoemsList.txt");
+        Assert.Equal(expectedResultAsString, resultAsString);
+    }
+
+
+    [Fact] //ATTENTION prend 22sec ! (167 poèmes)
+    public void Parse_WhenCalled_ShouldReturnTheCorrecteParseResult_From_LPDP_Marco_PoemsList()
+    {
+        Assert.True(true); return;
+        //--- Arrange ---
+        string stringToParse = File.ReadAllText($"{Fixtures.Pathes.FilesToParse_SubPath}/LPDP_Marco_PoemsList.html", Encoding.Latin1);
+        CharSequenceParser parser = Fixtures.LPDP_PoemsList.Parser(stringToParse);
+
+        CharBlocks expectedInnerBlocks = Fixtures.LPDP_PoemsList.expectedInnerBlocks;
+
+
+        //--- Act ---
+        ParseResult<char> parseResult = parser.Parse(expectedInnerBlocks);
+        string resultAsString = parseResult.GetStateAsString();
+        //File.WriteAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_From_LPDP_Marco_PoemsList.txt", resultAsString);
+
+        //--- Assert ---
+        string expectedResultAsString = File.ReadAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_From_LPDP_Marco_PoemsList.txt");
+        Assert.Equal(expectedResultAsString, resultAsString);
+    }
+
+    [Fact] //ATTENTION prend 10mn !!! (767 poèmes)
+    public void Parse_WhenCalled_ShouldReturnTheCorrecteParseResult_From_LPDP_Mystic4Ever_PoemsList()
+    {
+        Assert.True(true); return;
+        //--- Arrange ---
+        string stringToParse = File.ReadAllText($"{Fixtures.Pathes.FilesToParse_SubPath}/LPDP_Mystic4Ever_PoemsList.html", Encoding.Latin1);
+        CharSequenceParser parser = Fixtures.LPDP_PoemsList.Parser(stringToParse);
+
+        CharBlocks expectedInnerBlocks = Fixtures.LPDP_PoemsList.expectedInnerBlocks;
+
+
+        //--- Act ---
+        ParseResult<char> parseResult = parser.Parse(expectedInnerBlocks);
+        string resultAsString = parseResult.GetStateAsString();
+        //File.WriteAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_From_LPDP_Mystic4Ever_PoemsList.txt", resultAsString);
+
+        //--- Assert ---
+        string expectedResultAsString = File.ReadAllText($"{Fixtures.Pathes.ParseResults_AsTxtFiles_SubPath}/ParseResult_From_LPDP_Mystic4Ever_PoemsList.txt");
+        Assert.Equal(expectedResultAsString, resultAsString);
+    }
+
+}
---
+++ TESTS/Transverse._Common.SequenceParsing.UnitTests/Char/Sequence/Parser/zzFixtures/Fixtures.cs
@@ -0,0 +1,167 @@
+﻿using System.Text.RegularExpressions;
+
+using Transverse._Common.SequenceParsing.Char;
+using Transverse._Common.SequenceParsing.Generic;
+
+namespace Transverse._Common.SequenceParsing.UnitTests.Char;
+
+internal class Fixtures
+{
+    internal static class Pathes
+    {
+        private const string Assets_SubPath = "._Assets";
+        internal const string ParseResults_AsTxtFiles_SubPath = $"{Assets_SubPath}/_ParseResults_AsTxtFiles";
+        internal const string FilesToParse_SubPath = $"{Assets_SubPath}/._FilesToParse";
+    }
+
+    internal static class CSharpCode
+    {
+        private static string espacesSautsLigneRegExp = "(" + Environment.NewLine + "|[ ])*";
+        private static string accoladeOuvranteRegExp = espacesSautsLigneRegExp + "[{]" + espacesSautsLigneRegExp;
+        private static string ifRegExp = "if[ ]*[(](.*)[)]" + accoladeOuvranteRegExp;
+        private static string classRegExp = "class[ ]*[A-Z]+" + accoladeOuvranteRegExp;
+
+        private static StringMatchingEvaluator stringMatchingEvaluatorByEquality = new StringMatchingEvaluatorByEquality();
+        private static StringMatchingEvaluator stringMatchingEvaluatorByRegExp = new StringMatchingEvaluatorByRegExp();
+
+        private static CharBlockDelimiter commentMultiLigStartDelimiter = CharBlockDelimiter.Create("/*", stringMatchingEvaluatorByEquality);
+        private static CharBlockDelimiter commentMultiLigEndDelimiter = CharBlockDelimiter.Create("*/", stringMatchingEvaluatorByEquality);
+        private static CharBlock commentMultiLig_ = CharBlock.Create(commentMultiLigStartDelimiter, commentMultiLigEndDelimiter);
+
+        private static CharBlockDelimiter commentMonoLigStartDelimiter = CharBlockDelimiter.Create("//", stringMatchingEvaluatorByEquality);
+        private static CharBlockDelimiter commentMonoLigEndDelimiter = CharBlockDelimiter.Create(Environment.NewLine, stringMatchingEvaluatorByEquality);
+        private static CharBlock commentMonoLig_ = CharBlock.Create("Comm.MonoLig.", commentMonoLigStartDelimiter, commentMonoLigEndDelimiter);
+
+        private static CharBlockDelimiter ifStartDelimiter = CharBlockDelimiter.Create(ifRegExp, stringMatchingEvaluatorByRegExp, false);
+        private static CharBlockDelimiter ifEndDelimiter = CharBlockDelimiter.Create("}", stringMatchingEvaluatorByEquality);
+        private static CharBlock if_ = CharBlock.Create("IF{...}", ifStartDelimiter, ifEndDelimiter,
+                                                CharBlocks.Create(new List<CharBlock> { commentMultiLig_, commentMonoLig_ }), canBeNestedWithinItSelf: true);
+
+        private static CharBlockDelimiter classStartDelimiter = CharBlockDelimiter.Create(classRegExp, stringMatchingEvaluatorByRegExp, false);
+        private static CharBlockDelimiter classEndDelimiter = CharBlockDelimiter.Create("}", stringMatchingEvaluatorByEquality);
+        private static CharBlock class_ = CharBlock.Create("CLASS {...}", classStartDelimiter, classEndDelimiter, CharBlocks.Create(new List<CharBlock> { commentMultiLig_, commentMonoLig_, if_ }));
+
+        internal static CharBlocks expectedInnerBlocks = CharBlocks.Create(new List<CharBlock> {
+            commentMultiLig_,
+            commentMonoLig_,
+            class_,
+            if_
+        });
+
+    }
+
+    internal static class XML
+    {
+        private static StringMatchingEvaluator stringMatchingEvaluatorByRegExp = new StringMatchingEvaluatorByRegExp();
+        private static StringMatchingEvaluator stringMatchingEvaluatorByEquality = new StringMatchingEvaluatorByEquality();
+
+        private static string tagPrefix = @"<[ ]*[A-Z0-9\-]+";
+        private static string monotagOuvrantRegExp = $"{tagPrefix} ";
+        private static string tagOuvrantRegExp = $"{tagPrefix}[ ]*>";  //Seul le tag ouvrant suffira, car celui de fin sera immédiatement calculé une fois celui de début trouvé.
+
+
+        private static string attributStartRegExp = @"[ ]*[a-z0-9]+[=][\""]";
+        private static CharBlockDelimiter attributStartDelimiter = CharBlockDelimiter.Create(attributStartRegExp, stringMatchingEvaluatorByRegExp, false);
+        private static CharBlockDelimiter attributEndDelimiter = CharBlockDelimiter.Create("\"", stringMatchingEvaluatorByEquality, false);
+        private static CharBlock attributBlock = CharBlock.Create("tagAttribute=\"...\"", attributStartDelimiter, attributEndDelimiter);
+
+
+        private static CharBlockDelimiter monotagStartDelimiter = CharBlockDelimiter.Create(monotagOuvrantRegExp, stringMatchingEvaluatorByRegExp, false);
+        private static CharBlockDelimiter monotagEndDelimiter = CharBlockDelimiter.Create("/>", stringMatchingEvaluatorByEquality, false);
+        private static CharBlock monotag_ = CharBlock.Create("<monotag ... />", monotagStartDelimiter, monotagEndDelimiter,
+                                                             expectedInnerBlocks: CharBlocks.Create(new List<CharBlock> { attributBlock }));
+        //-- Ici on va exploiter un puissant mécanisme : EndDelimiter calculé en fonction du StartDelimiter réel trouvé (expr. régulière) --
+        // (cf. notamment : méthode OnStartDelimiterMatching_MayNeedANewAdaptedBlock de la classe CharBlock).
+        public static DelimiterComputer fComputeEndDelimiter = (string realStartDelimiter) =>   //realStartDelimiter donc ici de la forme : <Nimporte>
+        {
+            string endDelimiter = Regex.Replace(realStartDelimiter, "<", "</"); //On déduit le Delimiter de fin à partir de celui de début !
+            return endDelimiter;
+        };
+        private static CharBlockDelimiter tagStartDelimiter = CharBlockDelimiter.Create(tagOuvrantRegExp, stringMatchingEvaluatorByRegExp, false);
+        private static CharBlockDelimiter tagEndDelimiter = CharBlockDelimiter.Create(fComputeEndDelimiter, stringMatchingEvaluatorByEquality, false);
+        private static CharBlock tag_ = CharBlock.Create("<tag>...</tag>", tagStartDelimiter, tagEndDelimiter,
+                                                         expectedInnerBlocks: CharBlocks.Create(new List<CharBlock> { monotag_ }),
+                                                         canBeNestedWithinItSelf: true);
+        
+        internal static CharBlocks expectedInnerBlocks = CharBlocks.Create(new List<CharBlock> { tag_, monotag_ });
+    }
+
+    internal static class LPDP_PoemsList
+    {
+        private const string NODE_TYPE_TITRE_SECTION = "TITRE_SECTION";
+        private const string NODE_TYPE_TITRE_ECRIT = "TITRE_ECRIT";
+        private const string NODE_TYPE_INFOS_ECRIT = "INFOS_ECRIT";
+
+        private const string LINK_TAG_HREF_PATTERN = "<a href=\"(.*cat=[0-9]+)\">";
+
+
+        private static StringMatchingEvaluator stringMatchingEvaluatorByRegExp = new StringMatchingEvaluatorByRegExp();
+        private static StringMatchingEvaluator stringMatchingEvaluatorByEquality = new StringMatchingEvaluatorByEquality();
+
+
+        private static CharBlockDelimiter aTitreEtUrlEcritStartDelimiter = CharBlockDelimiter.Create($"{LINK_TAG_HREF_PATTERN}", stringMatchingEvaluatorByRegExp, false);
+        private static CharBlockDelimiter aTitreEtUrlEcritEndDelimiter = CharBlockDelimiter.Create("</a>", stringMatchingEvaluatorByEquality, false);
+        private static CharBlock aTitreEtUrlEcrit = CharBlock.Create($"{NODE_TYPE_TITRE_ECRIT} <a href='urlecrit'>titreEcrit</a>", aTitreEtUrlEcritStartDelimiter, aTitreEtUrlEcritEndDelimiter);
+
+        private static CharBlockDelimiter spanAutresInfosEcritStartDelimiter = CharBlockDelimiter.Create("<span class=\"medTxt\">", stringMatchingEvaluatorByEquality, false);
+        private static CharBlockDelimiter spanAutresInfosEcritEndDelimiter = CharBlockDelimiter.Create("</span>", stringMatchingEvaluatorByEquality, false);
+        private static CharBlock spanAutresInfosEcrit = CharBlock.Create($"{NODE_TYPE_INFOS_ECRIT} <span class='...'>autresInfos</span>", spanAutresInfosEcritStartDelimiter, spanAutresInfosEcritEndDelimiter);
+
+        private static CharBlockDelimiter simpleTdStartDelimiter = CharBlockDelimiter.Create("<td>", stringMatchingEvaluatorByEquality, false);
+        private static CharBlockDelimiter simpleTdEndDelimiter = CharBlockDelimiter.Create("</td>", stringMatchingEvaluatorByEquality, false);
+        private static CharBlock simpleTd = CharBlock.Create("<td>...</td>", simpleTdStartDelimiter, simpleTdEndDelimiter,
+                                                              expectedInnerBlocks: CharBlocks.Create(new List<CharBlock> { aTitreEtUrlEcrit, spanAutresInfosEcrit })
+                                                             );
+
+        private static CharBlockDelimiter tdTitreSectionStartDelimiter = CharBlockDelimiter.Create("<td class=\"msg0 msgTopBorderMed msgBottomBorderMed ti5-k\">", stringMatchingEvaluatorByEquality, false);
+        private static CharBlockDelimiter tdTitreSectionEndDelimiter = CharBlockDelimiter.Create("</td>", stringMatchingEvaluatorByEquality, false);
+        private static CharBlock tdTitreSection = CharBlock.Create($"{NODE_TYPE_TITRE_SECTION} <td class Titre section...>...</td>", tdTitreSectionStartDelimiter, tdTitreSectionEndDelimiter);
+
+        private static CharBlockDelimiter trStartDelimiter = CharBlockDelimiter.Create("<tr>", stringMatchingEvaluatorByEquality, false);
+        private static CharBlockDelimiter trEndDelimiter = CharBlockDelimiter.Create("</tr>", stringMatchingEvaluatorByEquality, false);
+        private static CharBlock trBlock = CharBlock.Create("<tr>...</tr>", trStartDelimiter, trEndDelimiter,
+                                                             expectedInnerBlocks: CharBlocks.Create(new List<CharBlock> { tdTitreSection, simpleTd })
+                                                            );
+
+        internal static CharBlocks expectedInnerBlocks = CharBlocks.Create(new List<CharBlock> { trBlock });
+
+        internal static CharSequenceParser Parser(string stringToParse) => GetParser(
+            stringToParse,
+            sequenceStartsAfterRegExp: new Regex("<table class=\"msg1 msgTableHeaderMed\""),
+            sequenceEndsAtRegExp: new Regex("<!-- END - Sous-tableau du centre")
+        );
+    }
+
+    internal static CharSequenceParser GetParser(string stringToParse, int sequenceStartIndex=0, int? sequenceEndIndex= null)
+    {
+        CharSequence charSequenceToParse = CharSequence.Create(stringToParse, sequenceStartIndex, sequenceEndIndex);
+        CharSequenceParser parser = GetParser(charSequenceToParse);
+        return parser;
+    }
+    internal static CharSequenceParser GetParser(string stringToParse, Regex sequenceStartsAfterRegExp, int? sequenceEndIndex = null)
+    {
+        CharSequence charSequenceToParse = CharSequence.Create(stringToParse, sequenceStartsAfterRegExp, sequenceEndIndex);
+        CharSequenceParser parser = GetParser(charSequenceToParse);
+        return parser;
+    }
+    internal static CharSequenceParser GetParser(string stringToParse, int sequenceStartIndex, Regex sequenceEndsAtRegExp)
+    {
+        CharSequence charSequenceToParse = CharSequence.Create(stringToParse, sequenceStartIndex, sequenceEndsAtRegExp);
+        CharSequenceParser parser = GetParser(charSequenceToParse);
+        return parser;
+    }
+
+    internal static CharSequenceParser GetParser(string stringToParse, Regex sequenceStartsAfterRegExp, Regex sequenceEndsAtRegExp)
+    {
+        CharSequence charSequenceToParse = CharSequence.Create(stringToParse, sequenceStartsAfterRegExp, sequenceEndsAtRegExp);
+        CharSequenceParser parser = GetParser(charSequenceToParse);
+        return parser;
+    }
+
+    private static CharSequenceParser GetParser(CharSequence charSequenceToParse)
+    {
+        CharSequenceParser parser = CharSequenceParser.Create(charSequenceToParse);
+        return parser;
+    }
+
+}
